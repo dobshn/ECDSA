64b227dbdd9399977b60fd238efa8f1a0c72a749
dobshn <dobshn@gmail.com>
2025-11-22 20:24:13 +0900

    initial commit

---
+++ ecdsa.c
@@ -0,0 +1,60 @@
+/*
+ * Copyright(c) 2020-2024 All rights reserved by Heekuck Oh.
+ * 이 프로그램은 한양대학교 ERICA 컴퓨터학부 학생을 위한 교육용으로 제작되었다.
+ * 한양대학교 ERICA 학생이 아닌 자는 이 프로그램을 수정하거나 배포할 수 없다.
+ * 프로그램을 수정할 경우 날짜, 학과, 학번, 이름, 수정 내용을 기록한다.
+ */
+#ifdef __linux__
+#include <bsd/stdlib.h>
+#elif __APPLE__
+#include <stdlib.h>
+#else
+#include <stdlib.h>
+#endif
+#include "ecdsa.h"
+#include "sha2.h"
+
+/*
+ * Initialize 256 bit ECDSA parameters
+ * 시스템파라미터 p, n, G의 공간을 할당하고 값을 초기화한다.
+ */
+void ecdsa_p256_init(void)
+{
+}
+
+/*
+ * Clear 256 bit ECDSA parameters
+ * 할당된 파라미터 공간을 반납한다.
+ */
+void ecdsa_p256_clear(void)
+{
+}
+
+/*
+ * ecdsa_p256_key() - generates Q = dG
+ * 사용자의 개인키와 공개키를 무작위로 생성한다.
+ */
+void ecdsa_p256_key(void *d, ecdsa_p256_t *Q)
+{
+}
+
+/*
+ * ecdsa_p256_sign(msg, len, d, r, s) - ECDSA Signature Generation
+ * 길이가 len 바이트인 메시지 m을 개인키 d로 서명한 결과를 r, s에 저장한다.
+ * sha2_ndx는 사용할 SHA-2 해시함수 색인 값으로 SHA224, SHA256, SHA384, SHA512,
+ * SHA512_224, SHA512_256 중에서 선택한다. r과 s의 길이는 256비트이어야 한다.
+ * 성공하면 0, 그렇지 않으면 오류 코드를 넘겨준다.
+ */
+int ecdsa_p256_sign(const void *msg, size_t len, const void *d, void *_r, void *_s, int sha2_ndx)
+{
+}
+
+/*
+ * ecdsa_p256_verify(msg, len, Q, r, s) - ECDSA signature veryfication
+ * It returns 0 if valid, nonzero otherwise.
+ * 길이가 len 바이트인 메시지 m에 대한 서명이 (r,s)가 맞는지 공개키 Q로 검증한다.
+ * 성공하면 0, 그렇지 않으면 오류 코드를 넘겨준다.
+ */
+int ecdsa_p256_verify(const void *msg, size_t len, const ecdsa_p256_t *_Q, const void *_r, const void *_s, int sha2_ndx)
+{
+}
